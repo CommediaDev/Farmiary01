c2aca2c6e83178394bc5f36ba1ed91a15fb47ed1
sda607 <64462106+sda607@users.noreply.github.com>
2022-11-14 15:18:55 +0900

    Update ItemRepositoryCustomImpl.java

--- src/main/java/com/shop/repository/ItemRepositoryCustomImpl.java
+++ src/main/java/com/shop/repository/ItemRepositoryCustomImpl.java
@@ -27,30 +27,29 @@ public class ItemRepositoryCustomImpl implements ItemRepositoryCustom{
         this.queryFactory = new JPAQueryFactory(em);
     }
 
-    private BooleanExpression searchSellStatusEq(ItemSellStatus searchSellStatus){
-        return searchSellStatus == null ? null : QItem.item.itemSellStatus.eq(searchSellStatus);
+    private BooleanExpression searchSellStatusEq(ItemSellStatus searchSellStatus){      //상품 판매 상태 조건이 전체(null)일 경우는 null을 리턴
+        return searchSellStatus == null ? null : QItem.item.itemSellStatus.eq(searchSellStatus);//판매중 or 품절 상태라면 해당 조건의 상품만 조회
     }
 
-    private BooleanExpression regDtsAfter(String searchDateType){
+    private BooleanExpression regDtsAfter(String searchDateType){//searchDateType의 값에 따라서 dateTime의 이저 시간으 값으로 세팅 후 해당 시간 이후로 등록된 상품만 조회
 
         LocalDateTime dateTime = LocalDateTime.now();
 
         if(StringUtils.equals("all", searchDateType) || searchDateType == null){
             return null;
-        } else if(StringUtils.equals("1d", searchDateType)){
+        } else if(StringUtils.equals("1d", searchDateType)){//1d: 최근 하루
             dateTime = dateTime.minusDays(1);
-        } else if(StringUtils.equals("1w", searchDateType)){
+        } else if(StringUtils.equals("1w", searchDateType)){//1w: 최근 일주일
             dateTime = dateTime.minusWeeks(1);
-        } else if(StringUtils.equals("1m", searchDateType)){
+        } else if(StringUtils.equals("1m", searchDateType)){//1m: 최근 한달
             dateTime = dateTime.minusMonths(1);
-        } else if(StringUtils.equals("6m", searchDateType)){
+        } else if(StringUtils.equals("6m", searchDateType)){//6m: 최근 6개월
             dateTime = dateTime.minusMonths(6);
         }
-
         return QItem.item.regTime.after(dateTime);
     }
 
-    private BooleanExpression searchByLike(String searchBy, String searchQuery){
+    private BooleanExpression searchByLike(String searchBy, String searchQuery){//상품명에 검색어를 포함하고 있는 상품, 상품 생성자으 아이디에 검색어를 포함
 
         if(StringUtils.equals("itemNm", searchBy)){
             return QItem.item.itemNm.like("%" + searchQuery + "%");
@@ -64,7 +63,7 @@ public class ItemRepositoryCustomImpl implements ItemRepositoryCustom{
     @Override
     public Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable) {
 
-        List<Item> content = queryFactory
+        List<Item> content = queryFactory   
                 .selectFrom(QItem.item)
                 .where(regDtsAfter(itemSearchDto.getSearchDateType()),
                         searchSellStatusEq(itemSearchDto.getSearchSellStatus()),
@@ -85,10 +84,11 @@ public class ItemRepositoryCustomImpl implements ItemRepositoryCustom{
         return new PageImpl<>(content, pageable, total);
     }
 
-    private BooleanExpression itemNmLike(String searchQuery){
+    //메인 페이지
+    private BooleanExpression itemNmLike(String searchQuery){   //검색어가 null이 아니면 상품명에 해당 검색어가 포함되는 상품을 조회하는 조건을 반환
         return StringUtils.isEmpty(searchQuery) ? null : QItem.item.itemNm.like("%" + searchQuery + "%");
     }
-
+    
     @Override
     public Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable) {
         QItem item = QItem.item;
@@ -96,7 +96,7 @@ public class ItemRepositoryCustomImpl implements ItemRepositoryCustom{
 
         List<MainItemDto> content = queryFactory
                 .select(
-                        new QMainItemDto(
+                        new QMainItemDto(       //QMainItemDto의 생성자에 반환할 값들을 넣어줌
                                 item.id,
                                 item.itemNm,
                                 item.itemDetail,
@@ -106,8 +106,8 @@ public class ItemRepositoryCustomImpl implements ItemRepositoryCustom{
                                 item.createdBy)
                 )
                 .from(itemImg)
-                .join(itemImg.item, item)
-                .where(itemImg.repimgYn.eq("Y"))
+                .join(itemImg.item, item)       //itemImg와 item을 내부 조인
+                .where(itemImg.repimgYn.eq("Y"))//상품 이미지의 경우 대표 상품 이미지만 불러옴
                 .where(itemNmLike(itemSearchDto.getSearchQuery()))
                 .orderBy(item.id.desc())
                 .offset(pageable.getOffset())
